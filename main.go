2bdac6193991fbdd06a120d2cf8b4ccc6ff5e61f
Tewodros Mitiku <tewodrosmitiku@Tewodross-MacBook-Pro.local>
2022-06-07 11:07:50 -0300

    add prerelease checks

--- main.go
+++ main.go
@@ -2,38 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
+
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/kurtosis-tech/stacktrace"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
-	"path"
-	"strconv"
-	"time"
 )
 
 const (
-	gitDirname = ".git"
-
-	// The name of the file inside the Git directory which will store when we last fetched (in Unix seconds)
-	lastFetchedFilename = "last-fetch.txt"
-
-	lastFetchedTimestampUintParseBase = 10
-	lastFetchedTimestampUintParseBits = 64
-
-	// How long we'll allow the user to go between fetches to ensure the repo is updated when they're releasing
-	fetchGracePeriod = 30 * time.Second
-
-	extraNanosecondsToAddToLastFetchedTimestamp = 0
-
-	lastFetchedFileMode = 0644
-
-	developBranchName = "develop"
-	masterBranchName  = "master"
-
+	gitDirname       = ".git"
 	originRemoteName = "origin"
+	masterBranchName = "master"
 )
 
 func main() {
@@ -46,58 +27,44 @@ func main() {
 
 func runMain() error {
 	// Check if we're in the root of a Git repo
-	currentWorkingDirpath, err := os.Getwd()
+	currentWorkingDirectory, err := os.Getwd()
 	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting the current working directory")
+		return stacktrace.Propagate(err, "An error occurred getting the current working directory.")
 	}
-	gitDirpath := path.Join(currentWorkingDirpath, gitDirname)
-	if _, err := os.Stat(gitDirpath); err != nil {
-		if os.IsNotExist(err); err != nil {
-			return stacktrace.NewError("No Git directory found, meaning this isn't being run from the root of a Git repo")
+	gitrepositoryDirectory := path.Join(currentWorkingDirectory, gitDirname)
+	if _, err := os.Stat(gitrepositoryDirectory); err != nil {
+		if os.IsNotExist(err) {
+			return stacktrace.Propagate(err, "An error occurred getting the git repositorysitory in this directory. This means that this binary is not being run from root of a git repositorysitory.")
 		}
-		return stacktrace.Propagate(err, "An unrecognized error occurred getting the Git directory at '%v'", gitDirpath)
 	}
-	repository, err := git.PlainOpen(currentWorkingDirpath)
+	repository, err := git.PlainOpen(currentWorkingDirectory)
 	if err != nil {
-		return stacktrace.Propagate(err, "Couldn't get a Git repo from the current repository")
+		return stacktrace.Propagate(err, "An error occurred while attempting to open the existing git repositorysitory.")
 	}
-	originRemote, err := repository.Remote(originRemoteName)
+
+	// Check local master branch exists
+	localMasterBranch, err := repository.Branch(masterBranchName)
 	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred getting remote '%v' for repository; is the code pushed?", originRemoteName)
+		return stacktrace.Propagate(err, "Missing required '%v' branch locally. Please run 'git checkout %v'", masterBranchName, masterBranchName)
+	}
+	fmt.Printf("Local master branch: %+v\n", localMasterBranch)
+
+	// Check no staged or unstaged changes exist on the branch before release
+	worktree, err := repository.Worktree()
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred while trying to retrieve the worktree of the repository.")
+	}
+	currWorktreeStatus, err := worktree.Status()
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred while trying to retrieve the status of the worktree of the repository.")
+	}
+	isClean := currWorktreeStatus.IsClean()
+	if !isClean {
+		fmt.Printf("The branch contains modified files. Please ensure the working tree is clean before attempting to release. Currently the status is '%s'\n", currWorktreeStatus)
+		return nil
 	}
 
-	config.LoadConfig()
-
-	// Fetch if needed
-	lastFetchedFilepath := path.Join(gitDirpath, lastFetchedFilename)
-	shouldFetch := determineShouldFetch(lastFetchedFilepath)
-	if shouldFetch {
-		if err := originRemote.Fetch(&git.FetchOptions{}); err != nil {
-			return stacktrace.Propagate(err, "An error occurred fetching from the remote repository")
-		}
-		currentUnixTimeStr := fmt.Sprint(time.Now().Unix())
-		if err := ioutil.WriteFile(lastFetchedFilepath, []byte(currentUnixTimeStr), lastFetchedFileMode); err != nil {
-			return stacktrace.Propagate(err, "An error occurred writing last-fetched timestamp '%v' to file '%v'", currentUnixTimeStr, lastFetchedFilepath)
-		}
-	}
-
-	// Compare that the local copies of
-	/*
-		developBranch, err := repository.Branch(developBranchName)
-		if err != nil {
-			return stacktrace.Propagate(err, "Missing required branch '%v' locally; you'll need to run 'git checkout %v'", developBranchName, developBranchName)
-		}
-
-		developBranch.Remote
-	*/
-
-	/*
-		masterBranch, err := repository.Branch(masterBranchName)
-		if err != nil {
-			return stacktrace.Propagate(err, "Missing required branch '%v' locally; you'll need to run 'git checkout %v'", masterBranchName, masterBranchName)
-		}
-	*/
-
+	// Check that local master and remote master are in sync
 	localMasterBranchName := masterBranchName
 	remoteMasterBranchName := fmt.Sprintf("%v/%v", originRemoteName, masterBranchName)
 
@@ -110,28 +77,11 @@ func runMain() error {
 		return stacktrace.Propagate(err, "An error occurred parsing revision '%v'", remoteMasterBranchName)
 	}
 
-	fmt.Println(localMasterHash.String())
-	fmt.Println(remoteMasterHash.String())
+	isLocalMasterInSyncWithRemoteMaster := localMasterHash.String() == remoteMasterHash.String()
+	if !isLocalMasterInSyncWithRemoteMaster {
+		fmt.Println("The local master branch is not in sync with the remote master branch. Must be in sync to conduct release process.")
+		return nil
+	}
 
 	return nil
-}
-
-func determineShouldFetch(lastFetchedFilepath string) bool {
-	lastFetchedUnixTimeStr, err := ioutil.ReadFile(lastFetchedFilepath)
-	if err != nil {
-		return true
-	}
-
-	lastFetchedUnixTime, err := strconv.ParseUint(
-		string(lastFetchedUnixTimeStr),
-		lastFetchedTimestampUintParseBase,
-		lastFetchedTimestampUintParseBits,
-	)
-	if err != nil {
-		logrus.Errorf("An error occurred parsing last-fetch Unix time string '%v':\n%v", err)
-	}
-	lastFetchedTime := time.Unix(int64(lastFetchedUnixTime), extraNanosecondsToAddToLastFetchedTimestamp)
-	noFetchNeededBefore := lastFetchedTime.Add(fetchGracePeriod)
-
-	return time.Now().After(noFetchNeededBefore)
 }
