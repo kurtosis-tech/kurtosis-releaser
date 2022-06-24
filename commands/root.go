49cf1b27915f5557020b0fef93ffba4addc6cb0c
Tedi Mitiku <tedi.m52@gmail.com>
2022-06-24 12:13:45 -0300

    update imports to rename kurtosis-releaser to kudet

--- commands/root.go
+++ commands/root.go
@@ -1,9 +1,9 @@
 package commands
 
 import (
-	"github.com/kurtosis-tech/kurtosis-releaser/commands/get-docker-tag"
-	"github.com/kurtosis-tech/kurtosis-releaser/commands/release"
-	"github.com/kurtosis-tech/kurtosis-releaser/commands/sed"
+	"github.com/kurtosis-tech/kudet/commands/get-docker-tag"
+	"github.com/kurtosis-tech/kudet/commands/release"
+	"github.com/kurtosis-tech/kudet/commands/sed"
 	"github.com/kurtosis-tech/stacktrace"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
